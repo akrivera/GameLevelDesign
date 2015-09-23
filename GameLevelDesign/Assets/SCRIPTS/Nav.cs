56a43328183292659ff9984b24b135619832a962
akrivera <akrivera1989@gmail.com>
2015-09-23 14:49:46 -0500

    Nav
    Nav Scene, nav script

---
+++ GameLevelDesign/Assets/SCRIPTS/Nav.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Nav : MonoBehaviour {
+	NavMeshAgent nav;
+	Transform target;
+	// Use this for initialization
+	void Start () {
+		nav = GetComponent<NavMeshAgent> ();
+		target = GameObject.FindGameObjectWithTag ("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		nav.destination = target.position;
+	}
+}
